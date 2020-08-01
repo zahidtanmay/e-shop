c1ea0179f3dd46a76cb45076ac3361beb4e09fa1
jahid <jahidul.islam@shohoz.net>
2020-08-01 21:32:59 +0600

    breadcrumb fixed

--- components/category/BreadCrumb.vue
+++ components/category/BreadCrumb.vue
@@ -1,6 +1,15 @@
 <template>
   <v-row>
     <v-breadcrumbs :items="breads">
+
+      <template v-slot:item="{item}">
+        <v-breadcrumbs-item
+          @click="routeLink(item.val, item.href)"
+          :class="item.disabled ? 'bread-crumb-link-disabled' : 'bread-crumb-link-active'"
+        >
+          {{item.text}}
+        </v-breadcrumbs-item>
+      </template>
       <template v-slot:divider>
         <v-icon>mdi-chevron-right</v-icon>
       </template>
@@ -30,16 +39,19 @@
               text: this.navLinks[cat]['text'],
               href: this.navLinks[cat]['link'],
               disabled: false,
+              val: { cat: cat, scat: null, chi: null }
             },
             {
               text: this.navLinks[cat]['child'][scat]['text'],
               href: this.navLinks[cat]['child'][scat]['link'],
               disabled: false,
+              val: { cat: cat, scat: scat, chi: null }
             },
             {
               text: this.navLinks[cat]['child'][scat]['child'][chi]['text'],
               href: this.navLinks[cat]['child'][scat]['child'][chi]['text'],
               disabled: true,
+              val: { cat: cat, scat: scat, chi: chi }
             }
 
           ]
@@ -49,11 +61,13 @@
               text: this.navLinks[cat]['text'],
               href: this.navLinks[cat]['link'],
               disabled: false,
+              val: { cat: cat, scat: null, chi: null }
             },
             {
               text: this.navLinks[cat]['child'][scat]['text'],
               href: this.navLinks[cat]['child'][scat]['link'],
               disabled: true,
+              val: { cat: cat, scat: scat, chi: null }
             }
           ]
         } else {
@@ -62,6 +76,7 @@
               text: parent.text,
               href: parent.link,
               disabled: true,
+              val: { cat: cat, scat: null, chi: null }
             }
           ]
         }
@@ -74,60 +89,28 @@
       bread: []
     }),
 
+    methods: {
 
+      routeLink (val, link) {
 
+        const currentNav = val
 
+        this.$router.push(`/${link}`)
+
+        this.$store.commit('nav/setCurrentNav', currentNav)
+
+      }
+
+    },
 
     async mounted() {
-      // console.log(this.currentNav, this.navLinks)
-      // const {cat, scat, chi} = await this.currentNav
-      // let parent = await Object.assign({}, this.navLinks[cat])
-      // console.log(this.currentNav, parent.text)
-      // let bread = []
-      // if (scat !=null && chi != null) {
-      //   bread = [
-      //     {
-      //       text: this.navLinks[cat]['text'],
-      //       href: this.navLinks[cat]['link'],
-      //       disabled: false,
-      //     },
-      //     {
-      //       text: this.navLinks[cat]['child'][scat]['text'],
-      //       href: this.navLinks[cat]['child'][scat]['link'],
-      //       disabled: false,
-      //     },
-      //     {
-      //       text: this.navLinks[cat]['child'][scat]['child'][chi]['text'],
-      //       href: this.navLinks[cat]['child'][scat]['child'][chi]['text'],
-      //       disabled: true,
-      //     }
-      //
-      //   ]
-      // } else if (scat != null) {
-      //   bread = [
-      //     {
-      //       text: this.navLinks[cat]['text'],
-      //       href: this.navLinks[cat]['link'],
-      //       disabled: false,
-      //     },
-      //     {
-      //       text: this.navLinks[cat]['child'][scat]['text'],
-      //       href: this.navLinks[cat]['child'][scat]['link'],
-      //       disabled: true,
-      //     }
-      //   ]
-      // } else {
-      //   bread = [
-      //     {
-      //       text: parent.text,
-      //       href: parent.link,
-      //       disabled: true,
-      //     }
-      //   ]
-      // }
-      //
-      // this.bread = bread
+
     }
 
   }
 </script>
+
+<style lang="scss">
+  .bread-crumb-link-disabled { color: rgba(0, 0, 0, 0.38); }
+  .bread-crumb-link-active { color: $nav-active-color; cursor: pointer;}
+</style>
